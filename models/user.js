eb0c3691ec389cdfcaa90dc2204db04260c312cd
vedrakholia32 <vedrakholia32@gmail.com>
2024-11-07 19:18:54 +0530

    1

--- models/user.js
+++ models/user.js
@@ -3,42 +3,22 @@ const bcrypt = require('bcrypt');
 
 // Define the erons schema
 const userSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true
+    firstName:{
+        type:String,
+        required:true
     },
-    age: {
-        type: Number
+    lastName:{
+        type:String,
+        required:true
     },
-    email: {
-        type: String,
-        required: true
+    email:{
+        type:String,
+        required:true,
+        unique:true
     },
-    mobile: {
-        type: String,
-        required: true
-    },
-    address: {
-        type: String,
-        required: true
-    },
-    aadharCardNumber: {
-        type: Number,
-        required: true,
-        unique: true
-    },
-    password: {
-        type: String,
-        required: true
-    },
-    role: {
-        type: String,
-        enum: ['voter', 'admin'],
-        default: 'voter'
-    },
-    isVoted: {
-        type: Boolean,
-        default: false
+    password:{
+        type:String,
+        required:true
     }
 
 });
--- server.js
+++ server.js
@@ -3,24 +3,16 @@ const cors = require('cors');
 const db = require('./db');
 const app = express();
 require('dotenv').config();
-app.use(cors({
-    origin: 'http://localhost:3000', // Allow only this origin
-    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed HTTP methods
-    credentials: true // If you need to include cookies or authentication headers
-  }));
-
+app.use(cors());
 
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 const PORT = process.env.PORT || 3000
 
 const userRoutes = require('./routes/userRoutes')
-const candidateRoutes = require('./routes/candidateRoutes')
 
 
 app.use('/user', userRoutes)
-app.use('/candidate', candidateRoutes)
-
 
 app.listen(3000, () => {
     console.log('listening on port 3000');
